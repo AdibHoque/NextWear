b9f7b3be9cb2ea842c24fbf33fc28c3a35baa41a
AdibHoque <adibhoque04@gmail.com>
2025-01-06 20:27:06 +0600

    Add Announcement topbar and fix loading not showing issue

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -13,7 +13,7 @@ import {
   Link,
   Input,
 } from "@nextui-org/react";
-import {CircleUserRound, Search} from "lucide-react";
+import {CircleUserRound, Search, X} from "lucide-react";
 import NavItems from "./NavItems";
 import {usePathname} from "next/navigation";
 import CartIcon from "./CartIcon";
@@ -21,98 +21,120 @@ import CartIcon from "./CartIcon";
 export default function Header() {
   const pathname = usePathname();
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+  const [notiOpen, setNotiOpen] = React.useState(true);
 
   const menuItems = ["Home", "Collections", "About", "Contact"];
 
   return (
-    <Navbar
-      classNames={{
-        item: [
-          "flex",
-          "relative",
-          "h-full",
-          "items-center",
-          "data-[active=true]:after:content-['']",
-          "data-[active=true]:after:absolute",
-          "data-[active=true]:after:bottom-0",
-          "data-[active=true]:after:left-0",
-          "data-[active=true]:after:right-0",
-          "data-[active=true]:after:h-[2px]",
-          "data-[active=true]:after:rounded-[2px]",
-          "data-[active=true]:after:bg-primary",
-        ],
-      }}
-      maxWidth="lg"
-      onMenuOpenChange={setIsMenuOpen}
-      isMenuOpen={isMenuOpen}
-      isBordered
-      shouldHideOnScroll
-    >
-      <NavbarContent>
-        <NavbarMenuToggle
-          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
-          className="sm:hidden"
-        />
-        <NavbarBrand>
-          <NextLink href="/">
-            <p className="font-bold text-inherit font-integral lg:text-2xl">
-              NextWear
+    <>
+      {notiOpen ? (
+        <span className="bg-black">
+          <div className="max-w-5xl mx-auto flex items-center justify-between px-4 animate-appearance-in">
+            <X className="text-black size-5" />
+            <p className="text-center text-white font-mono font-thin text-xs md:text-sm my-2">
+              Use code ADIB20 for 20% off any order.{" "}
+              <span className="font-normal">Shop Now!</span>
             </p>
-          </NextLink>
-        </NavbarBrand>
-      </NavbarContent>
-
-      <NavbarContent
-        className="hidden sm:flex gap-4 font-satoshi uppercase"
-        justify="center"
-      >
-        <NavItems />
-      </NavbarContent>
-      <NavbarContent justify="end">
-        {pathname !== "/collections" && (
-          <NextLink href="/collections">
-            <Input
-              classNames={{
-                base: "max-w-full sm:max-w-[10rem] h-10 hidden lg:flex",
-                mainWrapper: "h-full",
-                input: "text-small",
-                inputWrapper:
-                  "h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20",
+            <X
+              className="text-white size-5 cursor-pointer"
+              onClick={() => {
+                setNotiOpen(!notiOpen);
               }}
-              placeholder="Search"
-              size="sm"
-              startContent={<Search size={18} />}
-              type="search"
             />
-          </NextLink>
-        )}
-
-        <NavbarItem className="">
-          <NextLink href="/cart">
-            <CartIcon />
-          </NextLink>
-        </NavbarItem>
-        <NavbarItem className="">
-          <Link href="#">
-            <CircleUserRound />
-          </Link>
-        </NavbarItem>
-      </NavbarContent>
-      <NavbarMenu className="flex flex-col justify-center items-center gap-6">
-        {menuItems.map((item, index) => (
-          <NavbarMenuItem key={`${item}-${index}`}>
-            <NextLink
-              onClick={() => setIsMenuOpen(false)}
-              className="w-full uppercase font-integral text-4xl hover:opacity-60"
-              href={`/${
-                item.toLocaleLowerCase() == "home" ? "" : item.toLowerCase()
-              }`}
-            >
-              {item}
+          </div>
+        </span>
+      ) : (
+        ""
+      )}
+      <Navbar
+        classNames={{
+          item: [
+            "flex",
+            "relative",
+            "h-full",
+            "items-center",
+            "data-[active=true]:after:content-['']",
+            "data-[active=true]:after:absolute",
+            "data-[active=true]:after:bottom-0",
+            "data-[active=true]:after:left-0",
+            "data-[active=true]:after:right-0",
+            "data-[active=true]:after:h-[2px]",
+            "data-[active=true]:after:rounded-[2px]",
+            "data-[active=true]:after:bg-primary",
+          ],
+        }}
+        maxWidth="lg"
+        onMenuOpenChange={setIsMenuOpen}
+        isMenuOpen={isMenuOpen}
+        isBordered
+        shouldHideOnScroll
+      >
+        <NavbarContent>
+          <NavbarMenuToggle
+            aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+            className="sm:hidden"
+          />
+          <NavbarBrand>
+            <NextLink href="/">
+              <p className="font-bold text-inherit font-integral lg:text-2xl">
+                NextWear
+              </p>
             </NextLink>
-          </NavbarMenuItem>
-        ))}
-      </NavbarMenu>
-    </Navbar>
+          </NavbarBrand>
+        </NavbarContent>
+
+        <NavbarContent
+          className="hidden sm:flex gap-4 font-satoshi uppercase"
+          justify="center"
+        >
+          <NavItems />
+        </NavbarContent>
+        <NavbarContent justify="end">
+          {pathname !== "/collections" && (
+            <NextLink href="/collections">
+              <Input
+                classNames={{
+                  base: "max-w-full sm:max-w-[10rem] h-10 hidden lg:flex",
+                  mainWrapper: "h-full",
+                  input: "text-small",
+                  inputWrapper:
+                    "h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20",
+                }}
+                placeholder="Search"
+                size="sm"
+                startContent={<Search size={18} />}
+                type="search"
+              />
+            </NextLink>
+          )}
+
+          <NavbarItem className="">
+            <NextLink href="/cart">
+              <CartIcon />
+            </NextLink>
+          </NavbarItem>
+          <NavbarItem className="">
+            <Link href="#">
+              <CircleUserRound />
+            </Link>
+          </NavbarItem>
+        </NavbarContent>
+        <NavbarMenu className="flex flex-col justify-center items-center gap-6">
+          {menuItems.map((item, index) => (
+            <NavbarMenuItem key={`${item}-${index}`}>
+              <NextLink
+                onClick={() => setIsMenuOpen(false)}
+                className="w-full uppercase font-integral text-4xl hover:opacity-60"
+                href={`/${
+                  item.toLocaleLowerCase() == "home" ? "" : item.toLowerCase()
+                }`}
+              >
+                {item}
+              </NextLink>
+            </NavbarMenuItem>
+          ))}
+        </NavbarMenu>
+      </Navbar>
+    </>
   );
 }
